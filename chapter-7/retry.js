b3d2214f14cbd433d8b4a7c498402d779925b443
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2023-11-23 15:19:48 +0000

    Finish Chapter 7 Exercise 1

---
+++ chapter-7/retry.js
@@ -0,0 +1,25 @@
+class MultiplicatorUnitFailure extends Error {}
+
+function primitiveMultiply(a, b) {
+  if (Math.random() < 0.2) {
+    return a * b;
+  } else {
+    throw new MultiplicatorUnitFailure("Klunk");
+  }
+}
+
+function reliableMultiply(a, b) {
+  for (;;) {
+    try {
+      return primitiveMultiply(a, b);
+    } catch (e) {
+      if (e instanceof MultiplicatorUnitFailure) {
+        console.log("Multiplicatior Unit Failure");
+      } else {
+        throw e;
+      }
+    }
+  }
+}
+
+console.log(reliableMultiply(8, 8));
